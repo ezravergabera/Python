920d89522bdb439e8f181e378685d4163f9c07a7
Ezra Vergabera <ezra.vergabera@gmail.com>
2023-11-13 18:07:26 +0800

    practicising lambda or anonymous functions

---
+++ lambda.py
@@ -0,0 +1,9 @@
+# functions
+# def add(x,y):
+#     return x+y
+# print(add(4,5))
+
+
+# lambda method or also called as anonymous functions.
+# BNF: print((lambda <input>: <operation>)(<args>))
+print((lambda x,y: x+y)(4,5))
