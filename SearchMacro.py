6012c4ceafa51980e6953b3471777ef8c29ac482
Ezra Vergabera <ezra.vergabera@gmail.com>
2023-12-12 00:45:55 +0800

    created a pseudo database using file methods where it checks whether the randomly generated word is in the database, if it is then it would generate another, adjusted the interval from 10 seconds to 7 seconds

--- SearchMacro.py
+++ SearchMacro.py
@@ -3,8 +3,38 @@ import keyboard as key
 import random
 import time as t
 import msvcrt as m
+import json
+from icecream import ic
 
-times = 33
+def generate_random(num):
+    word = ""
+    for j in range(1, num + 1):
+        word = word + chr(random.randint(65, 122))
+        if random.randint(1, 2) == 1:
+            word = word + " "
+    return word
+
+def in_db(word):
+    with open("db.txt", "r") as f:
+        try:
+            db_set = json.loads(f.read())
+        except json.decoder.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            db_set = None
+
+    with open("db.txt", "w") as f:
+        if db_set == None:
+            db_set = [word]
+            f.write(ic(json.dumps(list(db_set))))
+            return False
+        elif word not in db_set:
+            db_set.append(word)
+            f.write(ic(json.dumps(list(db_set))))
+            return False
+        else:
+            return True
+
+times = 2
 
 print("Press any key to continue...")
 m.getch()
@@ -12,15 +42,14 @@ t.sleep(1)
 
 print("Press q to stop.")
 for i in range(1, times + 1):
-    t.sleep(10)
+    t.sleep(7)
     if(key.is_pressed("q")):
         break
     search = ""
     number_of_letter = random.randint(6, 15)
-    for j in range(1, number_of_letter + 1):
-        search = search + chr(random.randint(65, 122))
-        if random.randint(1, 2) == 1:
-            search = search + " "
+    search = generate_random(number_of_letter)
+    while in_db(search) == True:
+        search = generate_random(number_of_letter)
     py.typewrite(search)
     py.press("enter")
     with py.hold("ctrl"):
