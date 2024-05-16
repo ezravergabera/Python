18299556f4c28ae05e6a0c1c7aae36383e481535
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-05-16 23:51:54 +0800

    Created the selection function for the genetic algorithm

--- TSPOX.py
+++ TSPOX.py
@@ -29,7 +29,7 @@ batangas_map = {
 
 
 parents = []
-
+fitness_scores = {}
 
 def generate_rand():
     parent = []
@@ -43,7 +43,8 @@ def generate_rand():
     else:
         generate_rand()
 
-def distance(parent):
+def fitness(parent):
+    fitness_score = {}
     total_distance = 0
     for i, gene in enumerate(parent):
         curr_lat, curr_lon = batangas_map[gene]
@@ -56,9 +57,71 @@ def distance(parent):
 
     print(f"Total distance: {total_distance}")
 
+    fitness_score[parent] = total_distance
+    return fitness_score
+
+def selection(fitness_scores, parent1=None, parent2=None):
+    # pag may binigay na current parents
+    if parent1 is not None and parent2 is not None:
+        best_parent1 = parent1
+        best_parent2 = parent2
+
+    # isa or wala, return none
+    if len(fitness_score) < 2:
+        return None, None
+
+    # pag dalawa lang yung parent, return agad
+    if len(fitness_score) == 2:
+        return min(fitness_scores.items(), key=lambda x: x[1]), max(fitness_scores.items(), key=lambda x: x[1])
+
+    # pag 10 items and below, piliin yung dalawang parent na may mataas na score
+    if len(fitness_score) <= 10: 
+        for i, parent, fitness_score in enumerate(fitness_scores):
+            if i == 0 and best_parent1 is None:
+                best_parent1 = parent
+                best_fitness_score1 = fitness_score
+
+            if i == 1 and best_parent2 is None:
+                best_parent2 = parent
+                best_fitness_score2 = fitness_score
+
+            if best_fitness_score1 is not None and best_fitness_score2 is not None:
+                if best_fitness_score1 < best_fitness_score2:
+                    if fitness_score < (best_fitness_score1 + (best_fitness_score1 * 0.05)):
+                        best_parent1 = parent
+                        best_fitness_score1 = fitness_score
+                elif best_fitness_score2 < best_fitness_score1:
+                    if fitness_score < (best_fitness_score2 + (best_fitness_score2 * 0.05)):
+                        best_parent2 = parent
+                        best_fitness_score2 = fitness_score
+
+        return best_parent1, best_parent2
+    # pag more than 10, kuha ng tournament size(10) from the population(fitness_scores) tas tsaka mag survival of the fittest
+    else:
+        tournament_size = random.sample(fitness_score.items(), 10)
+        for i, parent, fitness_score in enumerate(tournament_size):
+            if i == 0 and best_parent1 is None:
+                best_parent1 = parent
+                best_fitness_score1 = fitness_score
+
+            if i == 1 and best_parent2 is None:
+                best_parent2 = parent
+                best_fitness_score2 = fitness_score
+
+            if best_fitness_score1 is not None and best_fitness_score2 is not None:
+                if best_fitness_score1 < best_fitness_score2:
+                    if fitness_score < (best_fitness_score1 + (best_fitness_score1 * 0.05)):
+                        best_parent1 = parent
+                        best_fitness_score1 = fitness_score
+                elif best_fitness_score2 < best_fitness_score1:
+                    if fitness_score < (best_fitness_score2 + (best_fitness_score2 * 0.05)):
+                        best_parent2 = parent
+                        best_fitness_score2 = fitness_score
+
+        return best_parent1, best_parent2
 parent = generate_rand()
 
-distance(parent)
+fitness_scores.update(fitness(parent))
 
 
 
