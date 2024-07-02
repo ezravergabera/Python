36ce2ea00028d2b5c3b5c6916e89223c62c5c46a
Jose Ezra Nazarene Vergabera <120149324+ezravergabera@users.noreply.github.com>
2024-07-03 00:02:47 +0800

    made the tkinter appear at center of screen

--- tk.py
+++ tk.py
@@ -18,6 +18,13 @@ window.title("Login Form")
 window.geometry("600x500")
 window.configure(bg="#333333")
 
+window.update_idletasks()
+width = window.winfo_width()
+height = window.winfo_height()
+x = (window.winfo_screenwidth() // 2) - (width // 2)
+y = (window.winfo_screenheight() // 2) - (height // 2)
+window.geometry(f'{width}x{height}+{x}+{y}')
+
 frame = tk.Frame(bg="#333333")
 
 login_label = tk.Label(frame, text="Login", bg='#333333', fg="#ff3399", font=("Arial", 30))
